b0fe6f20133feee66c588419d8d8091dd1b1971a
Greg Meyers <greg.meyers.1138@gmail.com>
2025-05-03 11:42:13 -0700

    Plug in real values for Cognito user pool ID and web client ID
    From aws --no-cli-pager cloudformation describe-stacks --stack-name jsc-tracker-cognito --query 'Stacks[0].Outputs' --output json

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,8 +1,10 @@
 const awsConfig = {
   Auth: {
     region: 'us-west-2',
-    userPoolId: 'YOUR_USER_POOL_ID', // Replace with actual value from CloudFormation output
-    userPoolWebClientId: 'YOUR_CLIENT_ID', // Replace with actual value from CloudFormation output
+    // From JSCTrackerCognitoUserPoolId
+    userPoolId: 'us-west-2_dMm3OHnHQ',
+    // From JSCTrackerCognitoUserPoolAppClientId
+    userPoolWebClientId: '4sinnfdl9rt41sskhage40qn7r',
     oauth: {
       domain: 'auth.jsc-tracker.infinitequack.net',
       scope: ['openid', 'profile', 'email', 'phone'],
