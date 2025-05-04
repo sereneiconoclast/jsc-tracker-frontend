3aaec2a4c02b1c381934bbfc5e2078fc5fb28e1f
Greg Meyers <greg.meyers.1138@gmail.com>
2025-05-03 18:43:49 -0700

    Set site-wide policy same-origin-allow-popups
    This was intended to prevent warnings in the JavaScript console ('Cross-Origin-Opener-Policy policy would block the window.closed call'), but it doesn't seem to work... and nor do these warnings seem to prevent the code flow from working

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups'
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'credentialless'
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
