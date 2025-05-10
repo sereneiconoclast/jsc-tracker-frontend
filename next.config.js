a72d5559ab11baa67738902842a65042af3d58a0
Greg Meyers <greg.meyers.1138@gmail.com>
2025-05-10 14:59:13 -0700

    Unused? next.config.js - I believe this serves no purpose without a Next server

--- next.config.js
+++ next.config.js
@@ -12,6 +12,18 @@ const nextConfig = {
           {
             key: 'Cross-Origin-Embedder-Policy',
             value: 'credentialless'
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'http://localhost:3000'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization'
           }
         ]
       }
